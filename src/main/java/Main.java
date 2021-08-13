a7385775706eeff2ddaef66281e0bb82be886133
Pieter <pieter.olaerts@gmail.com>
2021-08-13 12:33:58 +0200

    project

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Pieter pieter = new Pieter();
+        System.out.println(pieter.getAge());
+    }
+}
---
+++ src/main/java/Pieter.java
@@ -0,0 +1,6 @@
+public class Pieter {
+
+    private final int age = 24;
+
+    public int getAge() { return age;}
+}
